e84cdaa4d18407298a55d5ed2269c80e5ac577f7
e1b19005 <e1b19005@oit.ac.jp>
2021-10-19 16:16:54 +0900

    入室処理の実装

--- src/main/java/oit/is/lec04/kaizi/mobpro04/controller/Sample41controller.java
+++ src/main/java/oit/is/lec04/kaizi/mobpro04/controller/Sample41controller.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.lec04.kaizi.mobpro04.model.Chamber;
+import oit.is.lec04.kaizi.mobpro04.model.ChamberMapper;
+//import oit.is.lec04.kaizi.mobpro04.model.ChamberUser;
+//import oit.is.lec04.kaizi.mobpro04.model.UserInfo;
+
 /**
  * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
  */
@@ -20,8 +25,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/sample4")
 public class Sample41controller {
 
+  @Autowired
+  ChamberMapper chamberMapper;
+
   @GetMapping("step1")
   public String sample41() {
     return "sample41.html";
   }
+
+  /**
+   * 指定したidをPATHパラメータで受け取り，そのidに対応するデータを取得して返す
+   *
+   * @param id
+   * @param model
+   * @return
+   */
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
+    return "sample41.html";
+  }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   *
+   *         Transactionalはメソッドでトランザクション処理を実施したい場合に付与する
+   *         このメソッドが開始するとトランザクションが開始され，メソッドが正常に終了するとDBへのアクセスが確定する（Runtime
+   *         errorなどで止まった場合はロールバックが行われる）
+   */
+  @PostMapping("step3")
+  @Transactional
+  public String sample43(@RequestParam Integer number, ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    Chamber chamber3 = new Chamber();
+    chamber3.setNumber(number);
+    chamber3.setUser(loginUser);
+    chamberMapper.insertChamber(chamber3);
+    model.addAttribute("chamber3", chamber3);
+    // System.out.println("ID:" + chamber3.getId());
+    return "sample41.html";
+  }
+
 }
